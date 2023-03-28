c6217ae4500f53c36482b942b86b2b77d32a6eb8
ramonamour <44301743+ramonamour@users.noreply.github.com>
2023-03-28 11:59:52 +0200

    added handlefunctions

--- script.js
+++ script.js
@@ -15,3 +15,52 @@ function handleTouch14() {
         document.querySelector(".container2").classList.remove("show");
     }, 500)
 }
+
+function handleTouch13() {
+    document.querySelector(".container2").classList.add("show");
+    setTimeout(()=> {
+        document.querySelector(".container2").classList.remove("show");
+    }, 500)
+}
+
+function handleTouch27() {
+    document.querySelector(".container2").classList.add("show");
+    setTimeout(()=> {
+        document.querySelector(".container2").classList.remove("show");
+    }, 500)
+}
+
+function handleTouch32() {
+    document.querySelector(".container2").classList.add("show");
+    setTimeout(()=> {
+        document.querySelector(".container2").classList.remove("show");
+    }, 500)
+}
+
+function handleTouch33() {
+    document.querySelector(".container2").classList.add("show");
+    setTimeout(()=> {
+        document.querySelector(".container2").classList.remove("show");
+    }, 500)
+}
+
+function handleTouch4() {
+    document.querySelector(".container2").classList.add("show");
+    setTimeout(()=> {
+        document.querySelector(".container2").classList.remove("show");
+    }, 500)
+}
+
+function handleTouch2() {
+    document.querySelector(".container2").classList.add("show");
+    setTimeout(()=> {
+        document.querySelector(".container2").classList.remove("show");
+    }, 500)
+}
+
+function handleTouch15() {
+    document.querySelector(".container2").classList.add("show");
+    setTimeout(()=> {
+        document.querySelector(".container2").classList.remove("show");
+    }, 500)
+}
